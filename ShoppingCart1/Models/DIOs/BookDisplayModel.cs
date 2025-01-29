351c592b7cc272ef0545454dab2d1d1cda2322fd
Gokul <gokulr2087@gmail.com>
2025-01-29 21:52:39 +0530

    Customized search option

--- ShoppingCart1/Models/DIOs/BookDisplayModel.cs
+++ ShoppingCart1/Models/DIOs/BookDisplayModel.cs
@@ -3,7 +3,11 @@
     public class BookDisplayModel
     { 
             public IEnumerable<Book> Books {get; set;}
-        public IEnumerable<Book>Genres {get; set;}
+        public IEnumerable<Genre>Genres {get; set;}
+
+        public string STerm { get; set; } = "";
+
+        public int GenreId { get; set; } = 0;
 
     }
 }
